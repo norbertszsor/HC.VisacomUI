8848730fbd38acb14ac9c1974df62391b649012c
JakSzatan <47736299+JakSzatan@users.noreply.github.com>
2021-04-28 21:23:23 +0200

    leftBar update

--- ClientApp/src/components/rightBar/RightBar.jsx
+++ ClientApp/src/components/rightBar/RightBar.jsx
@@ -6,14 +6,14 @@ const RightBar = () =>{
         <div className="rightcolumn">
         <div className="card">
             <h2>Abaut us</h2>
-            <div className="fakeimg" style={{"height":"100px"}}>Image</div>
+            <div className="fakeimg" style={{"height":"100px"}}><img src={Faker.image.cats()} alt="" className="PopularPlantsImg"/></div>
             <p>{Faker.random.words(10)}</p>
         </div>
         <div className="card">
             <h3>Popular Plants</h3>
-            <div className="fakeimg"><p>Image</p></div>
-            <div className="fakeimg"><p>Image</p></div>
-            <div className="fakeimg"><p>Image</p></div>
+            <div className="fakeimg"><img src={Faker.image.cats()} alt="" className="PopularPlantsImg"/></div>
+            <div className="fakeimg"><img src={Faker.image.people()} alt="" className="PopularPlantsImg"/></div>
+            <div className="fakeimg"><img src={Faker.image.abstract()} alt="" className="PopularPlantsImg"/></div>
         </div>
         <div className="card">
             <h3>Our Gallery</h3>
