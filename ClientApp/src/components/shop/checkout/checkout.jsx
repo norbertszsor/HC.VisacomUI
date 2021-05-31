b18d205253ea44f28078c452dd4ad2f168b240ec
norbertszsor <47736350+norbertszsor@users.noreply.github.com>
2021-05-31 16:44:05 +0200

    checkout prototype working

--- ClientApp/src/components/shop/checkout/checkout.jsx
+++ ClientApp/src/components/shop/checkout/checkout.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import './checkout.css';
+import ProductItem from '../shopCart/ProductItem';
 export default class ShopCartRender extends React.Component{
 
   state = {
     user: null,
+    userAddresses: null,
   }
   async componentDidMount(){
     const key =localStorage.getItem('Veryfi');
@@ -11,12 +14,18 @@ export default class ShopCartRender extends React.Component{
         headers: {'accept': '*/*','Content-Type': 'application/json','Access-Control-Allow-Origin':" *","Authorization": "Bearer "+ key }
     }
     const apiUrl = "https://okiplants.azurewebsites.net/api/account/Details";
+    const apiUrlSecond = "https://okiplants.azurewebsites.net/api/address/getmyaddresses";
+    
     const rensponse = await fetch(apiUrl,requestOptions);
-    console.log(rensponse);
+    const rensponseSecond = await fetch(apiUrlSecond,requestOptions);
+    //console.log(rensponse);
     const data = await rensponse.json();
+    const dataSecond = await rensponseSecond.json();
     
     this.setState({user: JSON.parse(JSON.stringify(data))});
-    console.log(this.state.user);
+    this.setState({userAddresses: JSON.parse(JSON.stringify(dataSecond))});
+    console.log(this.state.userAddresses);
+    //console.log(this.state.user);
   }
 
   
@@ -24,25 +33,22 @@ export default class ShopCartRender extends React.Component{
     const key =localStorage.getItem('Veryfi');
     const apiUrl = "https://okiplants.azurewebsites.net/api/orders/add";
     var cartData = JSON.parse(localStorage.getItem("cartData"));
-    var productId = [];
-    var checkOutData = [];
+    var product = [];
+    var checkOutData = {
+        product: null,
+        addressId: null,
+    };
   
     cartData.forEach((element,index)=>{
-        productId.push({
+        product.push({
           id: element.id,
-          quantity: element.quantity,
+          amount: element.quantity,
         }
         )
     });
-    checkOutData.push(
-      {
-        productId,
-      },
-      {
-        addressId: addressUser,
-      } 
-    )
-    console.log(JSON.stringify(checkOutData));
+      checkOutData.product = product;
+      checkOutData.addressId = addressUser;
+    //console.log(JSON.stringify(checkOutData));
 
     
 
@@ -53,13 +59,16 @@ export default class ShopCartRender extends React.Component{
     }
 
     const rensponse = fetch(apiUrl,requestOptions);
-    console.log(rensponse);
-    console.log(key);
+    //console.log(rensponse);
+    //console.log(key);
+    //console.log(JSON.stringify(checkOutData));
     
   }
   
  
   render(){
+    const itemList = [];
+    var cartData = JSON.parse(localStorage.getItem("cartData"));
     
     if(this.state.isLoading){
       return(<div>loading</div>);
@@ -68,6 +77,19 @@ export default class ShopCartRender extends React.Component{
       return(<div>backed guys have mind error</div>);
    }
     if(this.state.user){
+      if(cartData!=null){
+        cartData.forEach((element,index) =>{
+            itemList.push(
+                <ProductItem 
+                    itemName={element.itemName} 
+                    quantity={element.quantity}
+                    plantImage={element.imageUrl}
+                    stockInfo ={element.itemStock}
+                    plantID = {element.plantID}
+                    />
+            )
+        });
+    }
     }
 
     if(localStorage.getItem('Veryfi')==null){
@@ -77,13 +99,36 @@ export default class ShopCartRender extends React.Component{
       
     }else{
       return(
-
-
         <div className="checkoutContainer">
-          <div>
-              <pre>{JSON.stringify(this.state.user)}</pre>
-          </div>
-          <button onClick={()=>this.handleClick(100)}>DodajTEST</button>
+            <div className="checkoutCartContainer">
+                  <div className="checkoutTxt">Your Cart</div>
+                  <div className="cartContainerItems">
+                    {itemList}
+                  </div>                
+                  <div className="checkoutTxt">Sum Price: NULL$</div>
+            </div>
+            <div className="checkoutFormContainer">
+                  <div className="checkoutTxt">First Name</div>
+                  <input type="text" className="checkoutInput"  value={this.state.user.firstName} readonly></input>
+                  <div className="checkoutTxt">Last Name</div>
+                  <input type="text" className="checkoutInput"  value={this.state.user.lastName} readonly></input>
+                  <div className="checkoutTxt">Your Email</div>
+                  <input type="text" className="checkoutInput"  value={this.state.user.email} readonly></input>
+                  <div className="checkoutTxt">Your Phone Number</div>
+                  <input type="text" className="checkoutInput"  value={this.state.user.phoneNumber} readonly></input>                 
+            </div>
+            <div className="paymentCheckout">
+                  <div className="checkoutTxt">Your Card Number</div>
+                  <input type="text" className="checkoutInput"  placeholder="0000-0000-0000-0000"></input>
+                  <div className="checkoutTxt">Name on Card</div>
+                  <input type="text" className="checkoutInput"  placeholder="Jhon Smith"></input>
+                  <div className="checkoutTxt">Exp. Date</div>
+                  <input type="text" className="checkoutInput"  placeholder="05/23"></input>
+                  <div className="checkoutTxt">CVV</div>
+                  <input type="text" className="checkoutInput"  placeholder="CVV: 000"></input>
+            </div>           
+            <button className="checkoutBtn"onClick={()=>this.handleClick(4)}>Finalize your purchases</button>
+          
         </div>
       )
     }
